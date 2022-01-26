fe17e404da008eaa62909400a1befa7576fac5fa
vivianrayzer <vivianrayzer.mg@gmail.com>
2022-01-26 14:49:48 -0300

    First Commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace R.Filmes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace R.Filmes
+{
+    class Program
+    {
+        static FilmeRepositorio repositorio = new FilmeRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarFilme();
+                        break;
+                    case "2":
+                        InserirFilme();
+                        break;
+                    case "3":
+                        AtualizarFilme();
+                        break;
+                    case "4":
+                        ExcluirFilme();
+                        break;
+                    case "5":
+                        VisualizarFilme();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ExcluirFilme()
+        {
+            Console.Write("Digite o id do Filme: ");
+            int indiceFilme = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceFilme);
+        }
+
+        private static void VisualizarFilme()
+        {
+            Console.Write("Digite o id do Filme: ");
+            int indiceFilme = int.Parse(Console.ReadLine());
+
+            var filme = repositorio.RetornaPorId(indiceFilme);
+
+            Console.WriteLine(filme);
+        }
+
+        private static void AtualizarFilme()
+        {
+            Console.Write("Digite o id do filme: ");
+            int indicefilme = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título do Filme: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início do Filme: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição do Filme: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Filme atualizaFilme = new Filme(id: indicefilme,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            repositorio.Atualiza(indicefilme, atualizaFilme);
+        }
+        private static void ListarFilme()
+        {
+            Console.WriteLine("Listar Filme");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma filme cadastrado.");
+                return;
+            }
+
+            foreach (var filme in lista)
+            {
+                var excluido = filme.retornaExcluido();
+
+                Console.WriteLine("#ID {0}: - {1} {2}", filme.retornaId(), filme.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+            }
+        }
+
+        private static void InserirFilme()
+        {
+            Console.WriteLine("Inserir novo Filme");
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título do filme: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início do filme: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição do filme: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Filme novoFilme = new Filme(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+
+            repositorio.Insere(novoFilme);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("#############################################################");
+            Console.WriteLine("\t\tBEM VINDO  a RAYZER FILMES!!");
+            Console.WriteLine("#############################################################");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar filmes");
+            Console.WriteLine("2- Inserir novo filme");
+            Console.WriteLine("3- Atualizar filme");
+            Console.WriteLine("4- Excluir filme");
+            Console.WriteLine("5- Visualizar filme");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
